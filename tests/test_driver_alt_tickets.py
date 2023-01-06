b5239558aa566af851e34b02464c6b73f04e80b5
shelby86 <shelby.nesterqatest@gmail.com>
2023-01-06 09:57:48 -0500

    drivers npt ticket

--- tests/test_driver_alt_tickets.py
+++ tests/test_driver_alt_tickets.py
@@ -6,6 +6,8 @@ from Endpoints.tickets import Tickets
 
 class TestDriverAltTickets:
 
+    load_types = FO.open_json_file(file_name='Data/load_types.json')
+
     @pytest.mark.driver_alt
     def test_driver_alt(self,base_url,default_headers,db):
         ticket_file = FO.open_json_file(file_name='Data/ticket.json')
@@ -24,7 +26,7 @@ class TestDriverAltTickets:
     def test_driver_alt_load_type_prod_prod(self,base_url,default_headers,db):
         ticket_file = FO.open_json_file(file_name='Data/ticket.json')
         ticket_file['TicketTypeId'] = 6
-        ticket_file['LoadTypeId'] = 7
+        ticket_file['LoadTypeId'] = self.load_types['prod_prod']
 
         ticket_id = Tickets.create_ticket(default_headers,base_url,file=ticket_file)
 
@@ -35,4 +37,57 @@ class TestDriverAltTickets:
 
         db_ticket = DB.query_runner_as_dict(db,query=sql)
         assert db_ticket['results'][0]['TicketTypeId'] == 6
-        assert db_ticket['results'][0]['LoadTypeId'] == 7
+        assert db_ticket['results'][0]['LoadTypeId'] == self.load_types['prod_prod']
+
+    @pytest.mark.driver_alt_load_type_comp_flo
+    def test_driver_alt_load_type_comp_flo(self,base_url,default_headers,db):
+        ticket_file = FO.open_json_file(file_name='Data/ticket.json')
+        ticket_file['TicketTypeId'] = 6
+        ticket_file['LoadTypeId'] = self.load_types['comp_flo']
+
+        ticket_id = Tickets.create_ticket(default_headers, base_url, file=ticket_file)
+
+        # Assert ticket in the DB
+        sql = f"""SELECT  TicketId,TicketTypeId,LoadTypeId
+                        FROM audit.Ticket
+                        WHERE TicketId = {ticket_id}"""
+
+        db_ticket = DB.query_runner_as_dict(db, query=sql)
+        assert db_ticket['results'][0]['TicketTypeId'] == 6
+        assert db_ticket['results'][0]['LoadTypeId'] == self.load_types['comp_flo']
+
+    @pytest.mark.driver_alt_facilities_fresh
+    def test_driver_alt_facilities_fresh(self,base_url,default_headers,db):
+        ticket_file = FO.open_json_file(file_name='Data/ticket.json')
+        ticket_file['TicketTypeId'] = 6
+        ticket_file['LoadTypeId'] = self.load_types['facilities_fresh']
+
+        ticket_id = Tickets.create_ticket(default_headers, base_url, file=ticket_file)
+
+        # Assert ticket in the DB
+        sql = f"""SELECT  TicketId,TicketTypeId,LoadTypeId
+                                FROM audit.Ticket
+                                WHERE TicketId = {ticket_id}"""
+
+        db_ticket = DB.query_runner_as_dict(db, query=sql)
+        assert db_ticket['results'][0]['TicketTypeId'] == 6
+        assert db_ticket['results'][0]['LoadTypeId'] == self.load_types['facilities_fresh']
+
+    @pytest.mark.driver_alt_load_type_drilling_fetch
+    def test_driver_alt_load_type_drilling_fetch(self,base_url,default_headers,db):
+        ticket_file = FO.open_json_file(file_name='Data/ticket.json')
+        ticket_file['TicketTypeId'] = 6
+        ticket_file['LoadTypeId'] = self.load_types['drilling_fetch']
+
+        ticket_id = Tickets.create_ticket(default_headers, base_url, file=ticket_file)
+
+        # Assert ticket in the DB
+        sql = f"""SELECT  TicketId,TicketTypeId,LoadTypeId
+                                        FROM audit.Ticket
+                                        WHERE TicketId = {ticket_id}"""
+
+        db_ticket = DB.query_runner_as_dict(db, query=sql)
+        assert db_ticket['results'][0]['TicketTypeId'] == 6
+        assert db_ticket['results'][0]['LoadTypeId'] == self.load_types['drilling_fetch']
+
+
